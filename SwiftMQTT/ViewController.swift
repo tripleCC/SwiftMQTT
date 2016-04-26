7a17a8c77c47afcd4115f66e8f397be758e9a5cd
tripleCC <triplec.linux@gmail.com>
2016-04-26 14:37:29 +0800

    Initial Commit

---
+++ SwiftMQTT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMQTT
+//
+//  Created by tripleCC on 16/4/26.
+//  Copyright © 2016年 tripleCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
